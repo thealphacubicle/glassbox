70e6222dbee15c8308fe91ab85a34bbccca42da1
Srihari Raman <raman.sr@northeastern.edu>
2025-08-28 15:57:51 -0400

    Bug fixes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="glassbox",
     version="0.1.0",
-    description="Your package description here",
-    author="Your Name",
+    description="Real-time observability for model training and hyperparameter tuning.",
+    author="Srihari Raman",
     packages=find_packages(),
     install_requires=[
         # Add your runtime dependencies here, e.g.:
